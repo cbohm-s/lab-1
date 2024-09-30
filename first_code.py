03237e75121604407f79bb3c09d00350b4b842ed
cbohm-s <s5708725@bournemouth.ac.uk>
2024-09-30 12:47:36 +0100

    Initial

---
+++ first_code.py
@@ -0,0 +1,35 @@
+print("Hello World!")
+print("Goodbye World!")
+print("Hello", "World", 2024)
+print("Hello", end=" ")
+print("World")
+
+name = "Alex"
+age = 20
+print(f"{name} is {age} years old")
+
+name = "Alice"
+print("Hello " + name)
+print("{} is {} years old".format("Alice", 30))
+
+print(342 + 699)
+print(200 - 49)
+print(604 / 4)
+print(20 * 5)
+print(3 % 2)
+print(9 // 3)
+print(8 // 3)
+print(5 ** 2)
+print(4 ** 3)
+
+a = 6
+b = 4
+c = 5
+d = 2
+e = 4
+f = 3
+
+result = (6 * 4) + (5 ** 2) - (4 % 3)
+
+print(f"(a * b) + (c ** d) - (e % f) = ", result)
+print(f"(a * b) + (c ** d) - (e % f) = ", result)
